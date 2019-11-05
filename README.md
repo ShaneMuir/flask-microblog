66a62341d853a56dbc5cf90d7f76265859dc95aa
Shane Muirhead <shane_muirhead@hotmail.co.uk>
2019-11-05 09:05:13 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Microblog
+
+Notes: 
+
+One Way
+$ [sudo]pip install virtualenv
+$ virtualenv --python=/usr/bin/python3 venv(or whatever name you want)
+$ source venv/bin/activate
+$ deactivate (to close your virtualenv)
+
---
+++ microblog.py
@@ -0,0 +1,18 @@
+from app import create_app, db, cli
+from app.models import User, Post
+
+app = create_app()
+cli.register(app)
+
+
+@app.shell_context_processor
+def make_shell_context():
+    return {'db': db, 'User': User, 'Post': Post}
+
+
+import os
+
+if __name__ == '__main__':
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
